82127a609011c91b01ed67cfb09e9d3426e29a85
Tuannurlaila katamo <tuannurlaila.k@ku.th>
2022-06-16 21:16:21 +0700

    test

--- API/Project/main.py
+++ API/Project/main.py
@@ -12,31 +12,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/my_name")
-def my_name():
-    data = "Susawatee Sama-ae"
-    return data
-
-
-@app.get("/input_name")
-def input_name(name):
-    data = name
-    return data
-
-
-@app.get("/input_name")
-def input_name(name, last_name):
-    data = "{} {}".format(name, last_name)
-    return data
-
-
-@app.get("/physics")
-def physics(s, t):
-    v = float(s) / float(t)
-    data = "v = {:.2f}".format(v)
-    return data
-
-
 @app.get("/get_hard_ware")
 def get_hard_ware():
     data = a.gethard_ware()
@@ -54,4 +29,4 @@ def inserthard_ware(name, hw_name):
     return data
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="192.168.0.105", port=80)
+    uvicorn.run(app, host="localhost", port=8000)
