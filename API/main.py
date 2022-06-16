865efe2b93d3c10ee81c2fa21289c48a775849c0
Susawatee Sama-ae <susawatee.s@ku.th>
2022-06-16 15:09:42 +0700

    past

--- API/main.py
+++ API/main.py
@@ -1,40 +1,57 @@
 from fastapi import FastAPI
 import uvicorn
 
+from action import Action
+a = Action
+
 app = FastAPI()
- 
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/my_name")
 def my_name():
     data = "Susawatee Sama-ae"
     return data
 
+
 @app.get("/input_name")
 def input_name(name):
     data = name
     return data
 
-@app.get("/input_name2")
-def input_name2(name,last_name):
-    data = "{} {}".format(name,last_name)
+
+@app.get("/input_name")
+def input_name(name, last_name):
+    data = "{} {}".format(name, last_name)
     return data
+
 
 @app.get("/physics")
-def physics(s,t):
-    v= float(s)/float(t)
-    data = "v= {:.2f}".format(v)
+def physics(s, t):
+    v = float(s) / float(t)
+    data = "v = {:.2f}".format(v)
     return data
 
-@app.get("/resistance_parallel_series")
-def resistance_parallel_series(R1,R2,R3):
-    parallel= float(R1)+float(R2)+float(R3),
-    series = float(1/R1)+float(1/R2)+float(1/R3)**-1
-    Rt = parallel,series
-    data = "Rt= {:.2f}".format(Rt)
+
+@app.get("/get_hard_ware")
+def get_hard_ware():
+    data = a.gethard_ware()
+    return data
+
+
+@app.get("/update_hard_ware")
+def update_hard_ware(ID, status):
+    data = a.updatehard_ware(ID, status)
+    return data
+
+@app.get("/inserthard_ware")
+def inserthard_ware(name, hw_name):
+    data = a.inserthard_ware(name, hw_name)
     return data
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="172.20.10.3", port=8080)
+    uvicorn.run(app, host="192.168.0.105", port=8080)
